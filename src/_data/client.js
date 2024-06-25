5f6a291e556045bea31700d0402735493f3ebc59
garcialexco <garcialexco@gmail.com>
2024-06-24 23:48:41 -0600

    Updated metadata

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,12 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
-    "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
-    },
+    "name": "Kendu Inu",
+    "email": "KenduInu@proton.me",
     "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "telegram": "https://t.me/KenduInu/",
+        "twitter": "https://x.com/kenduinu/",
+        "reddit": "https://www.reddit.com/r/KenduInu_Ecosystem/",
+        "youtube": "https://www.youtube.com/@KenduInuArmy/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.kenduinu.com"
 };
