ae4ca19e67a2bbf1411af790dd0595720f96f26a
garcialexco <garcialexco@gmail.com>
2024-07-03 00:27:25 -0600

    Carousel

--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -55,4 +55,64 @@ for (const item of faqItems) {
     };
     item.addEventListener('click', onClick);
 }
-                                
+
+//JS for carousel
+
+document.addEventListener('DOMContentLoaded', () => {
+  const slidesData = [
+    { src: 'https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/gym-service-1.png', description: 'Aerobic', link: '#' },
+    { src: 'https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/gym-service-2.png', description: 'Training', link: '#' },
+    { src: 'https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/gym-service-3.png', description: 'Body Builder', link: '#' },
+    { src: 'https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/gym-service-5.png', description: 'Yoga', link: '#' },
+    { src: 'https://csimg.nyc3.cdn.digitaloceanspaces.com/Images/People/gym-service-4.png', description: 'Cycling', link: '#' },
+  ];
+
+  const carouselTrack = document.querySelector('.carousel-track');
+
+  const createSlide = ({ src, description, link }) => {
+    const slideElement = document.createElement('div');
+    slideElement.className = 'slide';
+
+    const linkElement = document.createElement('a');
+    linkElement.href = link;
+
+    const imgElement = document.createElement('img');
+    imgElement.src = src;
+    imgElement.alt = description;
+
+    const overlayElement = document.createElement('div');
+    overlayElement.className = 'overlay';
+    overlayElement.textContent = description;
+
+    linkElement.append(imgElement, overlayElement);
+    slideElement.appendChild(linkElement);
+
+    return slideElement;
+  };
+
+  const populateCarouselTrack = (slides) => {
+    const fragment = document.createDocumentFragment();
+    slides.forEach(slide => fragment.appendChild(createSlide(slide)));
+
+    // Duplicate slides to create a seamless loop effect
+    slides.forEach(slide => fragment.appendChild(createSlide(slide)));
+
+    carouselTrack.appendChild(fragment);
+
+    const slideWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--slide-width'));
+    const gapWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--gap-width'));
+    const totalWidth = (slideWidth + gapWidth) * slides.length;
+    const duplicatedTotalWidth = totalWidth * 2;
+    const halfTotalWidth = duplicatedTotalWidth / 2;
+
+    carouselTrack.style.setProperty('--total-width', `${halfTotalWidth}px`);
+
+    const baseDuration = 40; // seconds | default = 40
+    const baseWidth = 5000; // px | default = 5000
+    const scrollDuration = (halfTotalWidth / baseWidth) * baseDuration;
+
+    carouselTrack.style.setProperty('--scroll-duration', `${scrollDuration}s`);
+  };
+
+  populateCarouselTrack(slidesData);
+});
