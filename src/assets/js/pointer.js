6603e9e286c2f7fadd28ba2e06b443395f7783bb
garcialexco <garcialexco@gmail.com>
2024-07-13 20:10:38 -0600

    Updates

--- src/assets/js/pointer.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('mousemove', function(e) {
-    var cursor = document.getElementById('customCursor');
-    cursor.style.left = e.pageX + 'px';
-    cursor.style.top = e.pageY + 'px';
-});
