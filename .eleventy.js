6aceef89ed05a04fcdf2f902adde482228f7f66f
mrW <ac@ria-systems.com>
2024-07-26 02:15:23 +0200

    removed eleventy-plugin-files-minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,6 @@
 // Imports
 const pluginEleventyNavigation = require("@11ty/eleventy-navigation");
-const pluginMinifier = require("@sherby/eleventy-plugin-files-minifier");
+// const pluginMinifier = require("@sherby/eleventy-plugin-files-minifier");
 const pluginSitemap = require("@quasibit/eleventy-plugin-sitemap");
 
 // Configs
@@ -63,7 +63,7 @@ module.exports = function (eleventyConfig) {
      *  https://github.com/benjaminrancourt/eleventy-plugin-files-minifier
      */
     if (isProduction) {
-        eleventyConfig.addPlugin(pluginMinifier);
+      //  eleventyConfig.addPlugin(pluginMinifier);
     }
     /**=====================================================================
                                 END PLUGINS
