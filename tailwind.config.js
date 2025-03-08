8fd97e801711af0eb43dfc694576a7f9b1105db9
mrW <ac@ria-systems.com>
2025-03-08 22:21:49 +0100

    tailwind cfg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,70 +3,66 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-    content: [
-        './**/*.{html,js}',
-        './**/*.html',
-        '!node_modules/**/*'
-    ],
+  content: ["./**/*.{html,js}", "./**/*.html", "!node_modules/**/*"],
 
-    theme: {
-        screens: {
-            xs: "480px",
-            sm: "768px",
-            md: "992px",
-            lg: "1200px",
-            xl: "1500px",
-        },
-
-        container: {
-            padding: {
-                DEFAULT: "20px",
-                sm: "1.5rem",
-                lg: "1.5rem",
-                xl: "1.5rem",
-                "2xl": "1.5rem",
-            },
-            center: true,
-        },
-
-        extend: {
-            fontFamily: {
-                headings: ["Teko Bold", "sans-serif"],
-                body: ["Sora", "sans-serif"]
-            },
-
-            colors: {
-                black: "#050505",
-                orange: "#E76449",
-                orange50: "#F3B1A4",
-                body: colors.white,
-            },
-            content: {
-                dark: colors.black,
-                light: colors.white,
-            },
-        },
-        corePlugins: {
-            container: false,
-        },
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "992px",
+      lg: "1200px",
+      xl: "1500px",
     },
 
-    plugins: [
-        function ({ addComponents }) {
-            addComponents({
-                ".container": {
-                    margin: "0 auto",
-                    "max-width": "100%",
-                    padding: "0 16px",
-                    width: "100%",
-                    "@screen sm": {
-                        padding: "0",
-                        width: "93.75%",
-                        "max-width": "1680px",
-                    },
-                },
-            });
+    container: {
+      padding: {
+        DEFAULT: "20px",
+        sm: "1.5rem",
+        lg: "1.5rem",
+        xl: "1.5rem",
+        "2xl": "1.5rem",
+      },
+      center: true,
+    },
+
+    extend: {
+      fontFamily: {
+        headings: ["Teko Bold", "sans-serif"],
+        body: ["Sora", "sans-serif"],
+      },
+
+      colors: {
+        black: "#050505",
+        orange: "#E76449",
+        orange50: "#F3B1A4",
+        body: colors.white,
+      },
+      content: {
+        dark: colors.black,
+        light: colors.white,
+      },
+    },
+    corePlugins: {
+      container: false,
+    },
+  },
+
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          margin: "0 auto",
+          "max-width": "100%",
+          padding: "0 16px",
+          width: "100%",
+          "@screen sm": {
+            padding: "0",
+            width: "93.75%",
+            "max-width": "1680px",
+          },
         },
-        require("@tailwindcss/container-queries"),
-    ],
+      });
+    },
+    require("@tailwindcss/container-queries"),
+  ],
 };
