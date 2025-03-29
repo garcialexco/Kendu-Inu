fc7cf0abb27fa076f2359e5db27b71ed5e2a190c
mrW <ac@ria-systems.com>
2025-03-29 23:27:17 +0100

    fmk..working

--- vite.config.js
+++ vite.config.js
@@ -1,60 +1,62 @@
-import path from "path";
-import { defineConfig } from "vite";
+import path from 'path';
+import { defineConfig } from 'vite';
+import globImporter from 'node-sass-glob-importer';
 
 export default defineConfig({
   build: {
-    outDir: "dist",
-    minify: "terser",
-    manifest: "manifest.json",
-    assetDir: "./assets",
+    outDir: 'dist',
+    minify: 'terser',
+    manifest: 'manifest.json',
+    assetDir: './assets',
     emptyOutDir: true,
     sourcemap: true,
     terserOptions: {
       mangle: {
-        reserved: ["_"],
+        reserved: ['_'],
       },
     },
     rollupOptions: {
       input: {
-        main: path.resolve(__dirname, "index.html"),
-        styles: path.resolve(__dirname, "assets/styles/main.scss"),
-        scripts: path.resolve(__dirname, "assets/scripts/main.js"),
+        main: path.resolve(__dirname, 'index.html'),
+        styles: path.resolve(__dirname, 'assets/styles/main.scss'),
+        scripts: path.resolve(__dirname, 'assets/scripts/main.js'),
       },
       output: {
-        entryFileNames: "assets/scripts/[hash].js",
-        chunkFileNames: "assets/scripts/[hash].js",
+        entryFileNames: 'assets/scripts/[hash].js',
+        chunkFileNames: 'assets/scripts/[hash].js',
         assetFileNames: ({ name }) => {
-          if (/\.css$/.test(name ?? "")) {
-            return "assets/styles/[hash].[ext]";
+          if (/\.css$/.test(name ?? '')) {
+            return 'assets/styles/[hash].[ext]';
           }
 
-          if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? "")) {
-            return "assets/images/[name].[ext]";
+          if (/\.(gif|jpe?g|png|svg|webp)$/.test(name ?? '')) {
+            return 'assets/images/[name].[ext]';
           }
 
-          if (/\.(woff2|woff|ttf|otf)$/.test(name ?? "")) {
-            return "assets/fonts/[name].[ext]";
+          if (/\.(woff2|woff|ttf|otf)$/.test(name ?? '')) {
+            return 'assets/fonts/[name].[ext]';
           }
 
-          return "assets/[name].[ext]";
+          return 'assets/[name].[ext]';
         },
       },
       cache: false,
     },
     optimizeDeps: {
-      include: ["assets/scripts/**/*.js"],
+      include: ['assets/scripts/**/*.js'],
     },
   },
   css: {
     preprocessorOptions: {
       scss: {
+        importer: globImporter(),
         quietDeps: true,
       },
     },
   },
   server: {
     watch: {
-      include: ["index.html", "assets/**/*"], // Watch the HTML file and asset directories
+      include: ['index.html', 'assets/**/*'], // Watch the HTML file and asset directories
     },
   },
 });
